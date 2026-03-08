0d1cc94b07954a4d5792f6975056d1e808ba6924
deepak <code.deepak9899sharma@gmail.com>
2026-03-08 21:01:38 +0530

    Updated header position issue | completed layout of skills sections

--- script.js
+++ script.js
@@ -1,9 +1,68 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let locoScroll;
+
 window.addEventListener("load", () => {
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
+
+  locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#html-wrapper"),
     smooth: true
   });
 
-  // Start at the top
-  scroll.scrollTo(0, { duration: 0 });
+  locoScroll.on("scroll", ScrollTrigger.update);
+   locoScroll.scrollTo(0, { duration: 0 });
+
+  ScrollTrigger.scrollerProxy("#html-wrapper", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight
+      };
+    },
+
+    pinType: document.querySelector("#html-wrapper").style.transform
+      ? "transform"
+      : "fixed"
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+
+
+
+  gsap.to(".loading-wrapper", {
+  delay: 3,
+  y: "-100%",
+  ease: "back",
+  duration: 2,
+  scale: 0.5,
+  opacity: 0,
+  display: "none",
+});
+
+// hero section
+gsap.to("#hero", {
+  y: -100,
+  scale: 0.5,
+  opacity: 0,
+
+  scrollTrigger: {
+    trigger: "#hero",
+    scroller: "#html-wrapper",
+    start: "top top",
+    end: "bottom top",
+    scrub: 2,
+  },
+});
+
+
 });

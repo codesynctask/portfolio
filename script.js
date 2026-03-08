8ef5ff5a668f9d2a5e8548a150df80267be7d2f2
deepak <code.deepak9899sharma@gmail.com>
2026-03-08 17:58:10 +0530

    updated hero sections with CTA btns

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true
+window.addEventListener("load", () => {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true
+  });
+
+  // Start at the top
+  scroll.scrollTo(0, { duration: 0 });
 });
